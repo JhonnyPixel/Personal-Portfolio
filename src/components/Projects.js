ef307423a0f7548b56ac59c5fc5a156b4aa1761e
JhonnyPixel <jhonnypixel@gmail.com>
2025-06-15 17:43:34 +0200

    aggiunta sezione app

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,7 +11,43 @@ import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
+import ratingo from "../assets/img/ratingo-screens.png"
+import crazypad from "../assets/img/launchpad_view.png"
+import alto from "../assets/img/alto.jpeg"
+import rapmonkey from "../assets/img/rapmonkey.png"
+import buzz from "../assets/img/iconBZZ.png"
+
 export const Projects = () => {
+
+
+  const appProjects = [
+    {
+      title: "Ratingo",
+      description: "Modern self-help app designed to support users in reducing porn addiction",
+      imgUrl: ratingo,
+    },
+    {
+      title: "Crazypad",
+      description: "dynamic launchpad app for iPhone that lets users create, mix, and perform music with just a few taps.",
+      imgUrl: crazypad,
+    },
+    {
+      title: "Alto",
+      description: "a sleek and minimalist app that lets you measure altitude with precision — entirely offline — using only your iPhone’s built-in barometer.",
+      imgUrl: alto,
+    },
+    {
+      title: "RapMonkey",
+      description: "an interactive app designed to help users learn and practice freestyle rap in a fun and intuitive way. ",
+      imgUrl: rapmonkey,
+    },
+    {
+      title: "Buzz",
+      description: " a safety-focused Apple Watch app designed to help prevent drowsiness while driving.",
+      imgUrl: buzz,
+    },
+    
+  ];
 
   const webProjects = [
     {
@@ -56,17 +92,34 @@ export const Projects = () => {
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
-                      <Nav.Link eventKey="first">WEB</Nav.Link>
+                      <Nav.Link eventKey="first">APP</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">GAMES</Nav.Link>
+                      <Nav.Link eventKey="second">WEB</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">DEVICES</Nav.Link>
+                      <Nav.Link eventKey="third">GAMES</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="four">DEVICES</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                     <Tab.Pane eventKey="first">
+                        <Row>
+                          {
+                            appProjects.map((project, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...project}
+                                />
+                              )
+                            })
+                          }
+                        </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
                       <Row>
                         {
                           webProjects.map((project, index) => {
@@ -80,7 +133,7 @@ export const Projects = () => {
                         }
                       </Row>
                     </Tab.Pane>
-                      <Tab.Pane eventKey="second">
+                      <Tab.Pane eventKey="third">
                         <Row>
                           {
                             gameProjects.map((project, index) => {
@@ -94,7 +147,7 @@ export const Projects = () => {
                           }
                         </Row>
                       </Tab.Pane>
-                    <Tab.Pane eventKey="third">
+                    <Tab.Pane eventKey="four">
                         <Row>
                           {
                             embedProjects.map((project, index) => {
@@ -108,6 +161,7 @@ export const Projects = () => {
                           }
                         </Row>
                     </Tab.Pane>
+                    
                   </Tab.Content>
                 </Tab.Container>
               </div>}
